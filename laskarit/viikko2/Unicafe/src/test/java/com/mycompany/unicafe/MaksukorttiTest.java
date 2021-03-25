712b7c236d784d158de541b4c18ea491eb04e2be
KirillosTY <57001633+KirillosTY@users.noreply.github.com>
2021-03-25 10:16:16 +0200

    testit ja raportti

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -1,8 +1,9 @@
 package com.mycompany.unicafe;
 
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MaksukorttiTest {
 
@@ -10,11 +11,57 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(15);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void alkuSaldo() {
+
+        assertTrue(kortti.saldo() == 15);
+    }
+
+    @Test
+    public void lataaminen() {
+        kortti.lataaRahaa(10);
+        assertTrue(kortti.saldo() == 25);
+    }
+
+    @Test
+    public void ottaminen() {
+
+        kortti.otaRahaa(3);
+        assertTrue(kortti.saldo()==12);
+
+    }
+
+    @Test
+    public void ottaminenNegaatiivinen() {
+
+        kortti.otaRahaa(30);
+        assertTrue(kortti.saldo()==15);
+
+    }
+
+    @Test
+    public void booleanPalautus() {
+
+        assertTrue(kortti.otaRahaa(10));
+    }
+
+    @Test
+    public void booleanPalautusNega() {
+
+        assertFalse(kortti.otaRahaa(100));
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("saldo: "+0+"."+15, kortti.toString());
+
     }
 }
